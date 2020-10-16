d459513e07ab46b0e5c2c69310c24cb4cf8b157c
hi <kangja1996@gmail.com>
2020-10-16 14:21:17 -0400

    progress

--- for-loops.js
+++ for-loops.js
@@ -5,19 +5,23 @@
 // for ([initialization]; [condition]; [final-expression]) {}
 // the loop continues to run until condition is false. final-expression is evaluated at the end of each loop iteration. It is usually used to increment/decrement a loop counter. 
 
-let ourArray = [];
-for (let i = 0; i < 5; i++) {
-  ourArray.push(i);
-}
+// let ourArray = [];
+// for (let i = 0; i < 5; i++) {
+//   ourArray.push(i);
+// }
 
-console.log(ourArray)
-[0, 1, 2, 3, 4]
+// console.log(ourArray)
+// [0, 1, 2, 3, 4]
 
 // **
 
 let ourArray = [];
 for (let i = 0; i < 5; i++) {
-  
+  if (i > 2) break; //use the break statement to break out of the loop early. If i is more than 2, break. After 2, i is more than 2 so it breaks out of the loop and we are DONE with the loop. 
+  ourArray.push(i);
 }
+
+console.log(ourArray)
+// [ 0, 1, 2 ]
 
 
