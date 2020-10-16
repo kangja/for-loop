fbc2a46c87e0c585e491770a59af31746baa1228
hi <kangja1996@gmail.com>
2020-10-16 14:07:17 -0400

    process

---
+++ for-loops.js
@@ -0,0 +1,23 @@
+// for loops.
+// we can run the same code multiple times using a loop. The most common loop in Javascript is called for loop because it runs for a specific number of times. 
+
+// Structure of a for loop
+// for ([initialization]; [condition]; [final-expression]) {}
+// the loop continues to run until condition is false. final-expression is evaluated at the end of each loop iteration. It is usually used to increment/decrement a loop counter. 
+
+let ourArray = [];
+for (let i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+console.log(ourArray)
+[0, 1, 2, 3, 4]
+
+// **
+
+let ourArray = [];
+for (let i = 0; i < 5; i++) {
+  
+}
+
+
